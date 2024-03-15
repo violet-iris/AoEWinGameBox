a9d38bc044f98c13500d1b45527b6979de36ea08
ideallove <20013535+ideallove@users.noreply.github.com>
2024-03-15 10:05:35 +0800

    fix CA1416 & update ViewModel

--- AoE4GameBox/Pages/PageUserInfo.xaml.cs
+++ AoE4GameBox/Pages/PageUserInfo.xaml.cs
@@ -2,20 +2,20 @@ using AoE4GameBox.Model;
 using AoE4GameBox.Tools;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
 
 namespace AoE4GameBox.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class PageUserInfo : Page
     {
+        private PageUserInfoViewModel VMUserInfo = new PageUserInfoViewModel();
+
         public PageUserInfo()
         {
             this.InitializeComponent();
+            this.DataContext = VMUserInfo;
         }
 
         /// <summary>
@@ -26,10 +26,14 @@ namespace AoE4GameBox.Pages
             Player PlayerInfo = await RequestAoE4World.GetPlayerClassBySearch(this.GetPlayerSearchText());
             if (PlayerInfo.Name != "Default")
             {
+                // 保存结果
+                VMUserInfo.PlayerName = $"玩家昵称 : {PlayerInfo.Name}";
+                VMUserInfo.PlayerId = $"玩家档案编号 (游戏ID) : {PlayerInfo.ProfileId}";
+                VMUserInfo.PlayerSteamId = $"Steam3 ID (64bit DEC) : {PlayerInfo.SteamId}";
                 // 输出到界面
-                text_player_name.Text = $"玩家昵称 : {PlayerInfo.Name}";
-                text_player_id.Text = $"玩家档案编号 (游戏ID) : {PlayerInfo.ProfileId}";
-                text_player_steam_id.Text = $"Steam3 ID (64bit DEC) : {PlayerInfo.SteamId}";
+                //text_player_name.Text = VMUserInfo.PlayerName;
+                //text_player_id.Text = VMUserInfo.PlayerId;
+                //text_player_steam_id.Text = VMUserInfo.PlayerSteamId;
                 // 反向绑定文本编辑框
                 this.TextBoxPlayerId.Text = PlayerInfo.ProfileId.ToString();
             }
---
+++ AoE4GameBox/Pages/PageUserInfoViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AoE4GameBox.Pages
+{
+    public class PageUserInfoViewModel : INotifyPropertyChanged
+    {
+        private string _playerName;
+        private string _playerId;
+        private string _playerSteamId;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string PlayerName
+        {
+            get { return _playerName; }
+            set
+            {
+                if (SetProperty(ref _playerName, value))
+                {
+                    // 在此处可添加在属性更改时需要执行的逻辑
+                }
+            }
+        }
+        public string PlayerId
+        {
+            get { return _playerId; }
+            set
+            {
+                if (SetProperty(ref _playerId, value))
+                {
+                    // 在此处可添加在属性更改时需要执行的逻辑
+                }
+            }
+        }
+        public string PlayerSteamId
+        {
+            get { return _playerSteamId; }
+            set
+            {
+                if (SetProperty(ref _playerSteamId, value))
+                {
+                    // 在此处可添加在属性更改时需要执行的逻辑
+                }
+            }
+        }
+
+        public PageUserInfoViewModel()
+        {
+            PlayerName = "玩家昵称";
+            PlayerId = "玩家档案编号 (游戏ID) ";
+            PlayerSteamId = "Steam3 ID (64bit DEC) ";
+        }
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!(object.Equals(field, newValue)))
+            {
+                field = (newValue);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
